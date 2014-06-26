ab9388f6f21c024db6b94c033ac628dcf8effc41
sean9keenan <sean9keenan@gmail.com>
2014-06-26 00:59:39 -0400

    Updated startup.sh

--- backend/startup.sh
+++ backend/startup.sh
@@ -3,4 +3,4 @@
 # This should be called from /etc/rc.local (that way it has sudo permission)
 
 # Restart the server process
-nohup sudo node /home/sean/Websites/NewWeb/backend/server.js > /home/sean/nohup/wordweb.out &
+nohup sudo node /home/sean/Websites/WordWeb/backend/server.js > /home/sean/nohup/wordweb.out &
